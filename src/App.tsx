35bbcebe408f840a59d16b01272f18ded952b8cb
Mike Field-May <michael.field-may@news.co.uk>
2019-11-29 13:50:55 +0000

    Add basic dom structure and styling to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import { Route } from "react-router-dom";
 import React, { Component } from "react";
 
-import { Users } from "./containers/Users";
-import { asyncComponent } from "./hoc/asyncComponent";
-import { LinkButton } from "./components/LinkButton/LinkButton";
-
-const AsyncPizza = asyncComponent(() => {
-  return import("./containers/Pizza");
-});
+import * as styles from './index.css'
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <LinkButton to="/" title="Users" />
-          <LinkButton to="/pizza" title="Pizza" />
+      <div className={styles.PokedexContainer}>
+        <div className={styles.PokedexHeader}>
+          <div className={styles.PokedexHeaderButtonContainer}>
+            <div className={styles.PokedexHeaderButtonRed}></div>
+            <div className={styles.PokedexHeaderButtonYellow}></div>
+            <div className={styles.PokedexHeaderButtonGreen}></div>
+          </div>
+          <div className={styles.PokedexHeaderTop}></div>
+          <div className={styles.PokedexHeaderLeft}>
+            <div className={styles.PokedexHeaderMainCircle}>
+              <div className={styles.PokedexHeaderInnerCircle}></div>
+            </div>
+          </div>
+          <div className={styles.PokedexHeaderCurve}></div>
         </div>
-        <div>
-          <Route path="/" exact component={Users} />
-          <Route path="/pizza" component={AsyncPizza} />
+        <div className={styles.PokedexScreenContainer}>
+          <div className={styles.PokedexScreenButtonWrapper}>
+            <div className={styles.PokedexScreenButton}></div>
+            <div className={styles.PokedexScreenButton}></div>
+          </div>
+          <div className={styles.PokedexScreen}></div>
+          <div className={styles.PokedexScreenBottom}>
+            <div className={styles.PokedexScreenBottomButton}></div>
+            <div className={styles.PokedexScreenBurgerMenu}>
+              <div className={styles.PokedexScreenBurgerMenuLine}></div>
+              <div className={styles.PokedexScreenBurgerMenuLine}></div>
+              <div className={styles.PokedexScreenBurgerMenuLine}></div>
+              <div className={styles.PokedexScreenBurgerMenuLine}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.PokedexBottom}>
+          <div className={styles.PokedexBottomButton}></div>
+          <div className={styles.PokedexMiniScreenContainer}>
+            <div className={styles.PokedexMiniScreenButtonContainer}>
+              <div className={styles.PokedexMiniScreenThinRedButton}></div>
+              <div className={styles.PokedexMiniScreenThinBlueButton}></div>
+            </div>
+            <div className={styles.PokedexMiniScreen}>
+              <p className={styles.PokedexScreenText}>Pikachu</p>
+            </div>
+          </div>
+          <div className={styles.PokedexTouchpadContainer}>
+            <div className={styles.PokedexTouchpadUp}></div>
+            <div className={styles.PokedexTouchpadLeft}></div>
+            <div className={styles.PokedexTouchpadMiddle}></div>
+            <div className={styles.PokedexTouchpadRight}></div>
+            <div className={styles.PokedexTouchpadBottom}></div>
+          </div>
         </div>
       </div>
     );
